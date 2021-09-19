40d945033ae249ae097cc536f53a1fd391b3e50e
Gustavo Angelo da Silva <gustavosilvaangelo@gmail.com>
2021-09-19 19:16:01 -0300

    novos comportamentos para a modal de tipo de documento

--- src/app/tipo-documento/modal-tipo-documento/modal-tipo-documento.component.ts
+++ src/app/tipo-documento/modal-tipo-documento/modal-tipo-documento.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { TipoDocumento } from 'src/app/shared/models/tipo-documento.model';
 import { TipoDocumentoService } from '../services/tipo-documento.service';
@@ -10,11 +11,24 @@ import { TipoDocumentoService } from '../services/tipo-documento.service';
 })
 export class ModalTipoDocumentoComponent implements OnInit {
 
+  @ViewChild('btnEditar') btnEditar!: ElementRef<HTMLInputElement>;
+  @ViewChild('btnExcluir') btnExcluir!: ElementRef<HTMLInputElement>;
   tiposDocumentos!: TipoDocumento[];
+  tipoDocumentoSelecionado: TipoDocumento | undefined;
 
-  constructor(public activeModal: NgbActiveModal, public tipoDocumentoService: TipoDocumentoService) { }
+  constructor(public activeModal: NgbActiveModal, private tipoDocumentoService: TipoDocumentoService) { }
 
   ngOnInit(): void {
     this.tiposDocumentos = this.tipoDocumentoService.listarTodos();
+  }
+
+  ativaBotoes(){
+    this.btnEditar.nativeElement.disabled = false;
+    this.btnExcluir.nativeElement.disabled = false;
+  }
+
+  setaSelecionado(evento: any, tipoDocumento : TipoDocumento): void{
+    this.tipoDocumentoSelecionado = tipoDocumento;
+    this.ativaBotoes();
   }
 }
--- src/app/tipo-documento/tipo-documento.module.ts
+++ src/app/tipo-documento/tipo-documento.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TipoDocumentoService } from './services/tipo-documento.service';
 import { ModalTipoDocumentoComponent } from './modal-tipo-documento/modal-tipo-documento.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -10,7 +11,8 @@ import { ModalTipoDocumentoComponent } from './modal-tipo-documento/modal-tipo-d
     ModalTipoDocumentoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   providers: [
     TipoDocumentoService
