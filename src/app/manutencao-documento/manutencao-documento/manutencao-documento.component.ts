4938f7cc4dc4b4324e08e6b0f6e4c3ee6861e4e9
Gustavo Angelo da Silva <gustavosilvaangelo@gmail.com>
2021-10-14 22:31:19 -0300

    Dinamicidade do tipo de documento e filtro seletor do agrupador de documentos na tela de manutenção de documentos

--- src/app/manutencao-documento/manutencao-documento/manutencao-documento.component.ts
+++ src/app/manutencao-documento/manutencao-documento/manutencao-documento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Documento } from 'src/app/shared/models/documento.model';
 import { TipoDocumento } from 'src/app/shared/models/tipo-documento.model';
@@ -9,21 +9,47 @@ import { DocumentoService } from '../services/documento.service';
   selector: 'app-manutencao-documento',
   templateUrl: './manutencao-documento.component.html',
   styleUrls: ['./manutencao-documento.component.css']
-})
+}) 
 export class ManutencaoDocumentoComponent implements OnInit {
   @ViewChild('formManutencaoDocumento') formManutencaoDocumento! : NgForm;
-  tipoDocumentos!: TipoDocumento[];
-  documento!: Documento;
+  filtroTipoDeDocumento!: TipoDocumento;
 
+  tipoDocumentos!: TipoDocumento[];
+  documentos!: Documento[];
+  documentoPrincipal!: Documento;
+  
   constructor(private tipoDocumentoService : TipoDocumentoService, private documentoService: DocumentoService) { }
 
+  filtraListaDocumentosPorTipoDeDocumento() : void {
+    if(this.filtroTipoDeDocumento != null)
+      this.documentos = this.documentoService.listarTodos().filter((documento) => documento.tipoDocumento!.id == this.filtroTipoDeDocumento.id);
+    else
+      this.documentos = this.documentoService.listarTodos();
+  }
+
   ngOnInit(): void {
+    this.preparaValoresIniciaisTela();
+    this.documentoPrincipal = new Documento();
+  }
+
+  preparaValoresIniciaisTela(): void {
+    this.preencheTiposDeDocumentos();
+    this.preencheDocumentos();
+  }
+
+  preencheTiposDeDocumentos(): void {
     this.tipoDocumentos = this.tipoDocumentoService.listarTodos();
-    this.documento = new Documento();
-    //this.documento = this.documentoService.buscarPorId(1634253576969);
+  }
+
+  preencheDocumentos(): void {
+    this.documentos = this.documentoService.listarTodos();
   }
 
   salvarDocumento(): void {
-    this.documentoService.inserir(this.documento!);
+    this.documentoService.inserir(this.documentoPrincipal!);
+  }
+
+  selecionarDocumentoPrincipal(documento : Documento){
+    this.documentoPrincipal = documento;
   }
 }
